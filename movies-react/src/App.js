ac85d07ff2062837e254ea1723c867e3cb16d9f3
sujoodaljundi <sujood.aljundi@gmail.com>
2024-12-05 23:11:18 +0300

    sidebar firt push

--- movies-react/src/App.js
+++ movies-react/src/App.js
@@ -1,8 +1,11 @@
-import "./App.css";
+// import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Register from "./components/Register";
 import Login from "./components/Login";
+import Sidebar from "./components/Sidebar/Sidebar";
+import './components/Sidebar/Sidebar.css';
+
 import "./assets/css/style.css";
 
 function App() {
@@ -10,8 +13,10 @@ function App() {
     <Router>
       <div>
         <Routes>
+          <Route path="/" element={<Sidebar/>}/>
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+
         </Routes>
       </div>
     </Router>
